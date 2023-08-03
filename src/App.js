6e7ebba8a794219be7d1c246f4b7c3ca18a81371
adifkhan <pkadif@gmail.com>
2023-08-03 17:34:21 +0600

    register and login page implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Register from './Pages/Register/Register';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='register' element={<Register />}></Route>
+        <Route path='login' element={<Login />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
